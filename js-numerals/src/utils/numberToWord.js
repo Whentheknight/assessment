b00f5b54f5fbee13c9b0f55f1decbd418a5a25c7
Whentheknight <szepzeno@gmail.com>
2022-02-06 19:44:54 +0100

    Write main switch logic for conversion

---
+++ js-numerals/src/utils/numberToWord.js
@@ -0,0 +1,34 @@
+export function numToWord(num){
+
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+                'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+                'seventeen', 'eighteen', 'nineteen'];
+
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+                'ninety'];
+
+    let numString = num.toString();
+    let numStringLen = numString.length;
+
+    switch(true){
+      //throw error when number is negative
+      case (num<0) : throw new Error('Negative numbers are not supported.');
+      //return zero if number is 0
+      case (num == 0) : return 'zero';
+      //case when number is lower than 20 
+      case (num < 20) : return ones[num];
+      //nested switch case for number bigger of equal to 20
+      case(num >= 20):
+        switch(numStringLen){
+            case 2: return inTens();
+            case 3: return inHundreds();
+            case 4: return inThousands();
+            case 5: return inTenThousands();
+            case 6: return inHundredThousands();
+            case 7: return inMillions();
+            case 8: return inTenMillions();
+            case 9: return inHundredMillions();
+        }
+        break;
+    }
+}
