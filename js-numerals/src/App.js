7bb5d0f14f585c71aa625701eebab5cdd8f27c07
Whentheknight <szepzeno@gmail.com>
2022-02-07 14:23:08 +0100

    Recreated app using npm create-react-app, because of test didn't work with Vite app

---
+++ js-numerals/src/App.js
@@ -0,0 +1,13 @@
+import Converter from './components/Converter';
+import './css/App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Converter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ js-numerals/src/components/Converter.jsx
@@ -0,0 +1,49 @@
+import { numToWord } from "../utils/numberToWord";
+import {useState} from 'react';
+import '../css/Converter.css';
+
+const Converter = () =>{
+    const [num, setNum] = useState("");
+    const [error, setError] = useState("");
+    const [converted, setConverted] = useState("");
+
+    function translate(event){
+        setNum(event.target.value)
+
+        try{
+            setError("")
+            setConverted(convert(num))
+            
+        } catch(error){
+            setConverted("")
+            setError(getErrorMsg(error))
+        }
+    } 
+
+    return(
+        <div className="card">
+        <div className="converterDiv">
+            <h1>Type in a number:</h1>
+            <input className="input" defaultValue="0" min={0} onChange={translate} value={num} type="number"></input> 
+            </div>
+
+            <div className="messages">
+                {error ? <span id="error">{error}</span> : null}
+            {converted ? <span id="converted">{converted}</span> : null}
+            
+        </div></div>)
+}
+
+export default Converter;
+
+//If number conversion can happen without an error
+function convert(num) {
+    let numInt = parseInt(num);
+
+    return numToWord(numInt);
+}
+
+//If error was thrown when conversion happened
+function getErrorMsg(e) {
+    return e.toString().replace('Error: ', '');
+}
